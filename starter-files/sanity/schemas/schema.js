2fd9377114196d3b07a001460b18c83398b242ee
Jennifer Dust <jennifer@atendesigngroup.com>
2020-10-30 10:11:18 -0500

    Created Person schema file

--- starter-files/sanity/schemas/schema.js
+++ starter-files/sanity/schemas/schema.js
@@ -4,6 +4,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 // Then we give our schema to the builder and provide the result to Sanity
 
+import person from './person';
 import pizza from './pizza';
 import topping from './topping';
 
@@ -12,5 +13,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([pizza, topping]),
+  types: schemaTypes.concat([person, pizza, topping]),
 });
