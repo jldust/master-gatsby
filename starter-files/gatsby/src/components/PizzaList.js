c28cb792018f09f56bb0db680f21e61944e7061e
Jennifer Dust <jennifer@atendesigngroup.com>
2020-11-14 14:21:24 -0600

    Finished section 5 adding queries

--- starter-files/gatsby/src/components/PizzaList.js
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Link from 'gatsby';
+import { Link } from 'gatsby';
 
 function SinglePizza({ pizza }) {
   return (
     <div>
       <Link to={`/pizza/${pizza.slug.current}`}>
-          
-                     <h2>{pizza.name}</h2>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
       </Link>
     </div>
   );
